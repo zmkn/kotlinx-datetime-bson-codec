bce1f8676091d7f1907af28a8e94e38f35c762cf
hz <beijingren@vip.qq.com>
2024-12-27 14:48:48 +0800

    初始化项目

---
+++ src/main/kotlin/com/zmkn/bson/codecs/InstantCodec.kt
@@ -0,0 +1,22 @@
+package com.zmkn.bson.codecs
+
+import kotlinx.datetime.Instant
+import org.bson.BsonReader
+import org.bson.BsonWriter
+import org.bson.codecs.Codec
+import org.bson.codecs.DecoderContext
+import org.bson.codecs.EncoderContext
+
+class InstantCodec : Codec<Instant> {
+    override fun encode(writer: BsonWriter, value: Instant, encoderContext: EncoderContext) {
+        writer.writeString(value.toString())
+    }
+
+    override fun getEncoderClass(): Class<Instant> {
+        return Instant::class.java
+    }
+
+    override fun decode(reader: BsonReader, decoderContext: DecoderContext): Instant {
+        return Instant.parse(reader.readString())
+    }
+}
